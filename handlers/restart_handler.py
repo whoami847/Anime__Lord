761e9760338e94baa54b6bcd1ee361aa4c9aba25
whoami847 <mrbeastbangla069@gmail.com>
2025-04-13 20:29:59 +0600

    Update restart_handler.py

--- handlers/restart_handler.py
+++ handlers/restart_handler.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 from utils.command_handler import CommandHandler
 import os
 import sys
@@ -6,8 +6,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-@filters.command("restart")
-@CommandHandler.admin_only
 async def restart_handler(client: Client, message):
     await message.reply("Restarting the bot...")
     logger.info("Bot restart initiated by admin.")
