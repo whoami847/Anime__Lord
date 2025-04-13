f2d0d9117bf911f9a5b937be0c77efe146421269
whoami847 <mrbeastbangla069@gmail.com>
2025-04-13 15:58:27 +0600

    Create help_handler.py

---
+++ handlers/help_handler.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from utils.message_formatter import MessageFormatter
+from utils.command_handler import CommandHandler
+from config import ABOUT_MESSAGE
+
+@filters.command("help")
+@CommandHandler.ban_check
+@CommandHandler.force_sub_check
+async def help_handler(client: Client, message):
+    text, _ = MessageFormatter.format_message(
+        f"{ABOUT_MESSAGE}\n\nSTILL HAVE DOUBTS, CONTACT BELOW PERSONS/GROUP AS PER YOUR NEED!"
+    )
+    buttons = [
+        [InlineKeyboardButton("Support Chat Group", url="https://t.me/support_chat_group")],
+        [InlineKeyboardButton("Owner", url="https://t.me/owner_username")],
+        [InlineKeyboardButton("Developer", url="https://t.me/shidoteshika1")]
+    ]
+    await message.reply(
+        text,
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
