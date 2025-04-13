ebe883841e57d5296d742560322bfc87f944554a
whoami847 <mrbeastbangla069@gmail.com>
2025-04-13 20:22:15 +0600

    Update ban_handler.py

--- handlers/ban_handler.py
+++ handlers/ban_handler.py
@@ -1,9 +1,7 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 from utils.command_handler import CommandHandler
 from database.user_manager import UserManager
 
-@filters.command("ban")
-@CommandHandler.admin_only
 async def ban_handler(client: Client, message):
     if not message.reply_to_message:
         await message.reply("Please reply to a user's message to ban them!")
@@ -15,8 +13,6 @@ async def ban_handler(client: Client, message):
     await message.reply(f"User {user_id} has been banned. Reason: {reason}")
     await client.send_message(user_id, f"You have been banned from the bot. Reason: {reason}")
 
-@filters.command("unban")
-@CommandHandler.admin_only
 async def unban_handler(client: Client, message):
     if not message.reply_to_message:
         await message.reply("Please reply to a user's message to unban them!")
